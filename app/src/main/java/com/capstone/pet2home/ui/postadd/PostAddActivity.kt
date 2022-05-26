1e021047b8cfc47797c9cd06ba3cc4f88e7b80dc
innoji26 <innoji@upi.edu>
2022-05-26 21:43:57 +0700

    menambahkan addPost dan customButton posting

--- app/src/main/java/com/capstone/pet2home/ui/postadd/PostAddActivity.kt
+++ app/src/main/java/com/capstone/pet2home/ui/postadd/PostAddActivity.kt
@@ -1,21 +1,150 @@
 package com.capstone.pet2home.ui.postadd
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBar
+import androidx.core.app.ActivityCompat
+import androidx.core.widget.addTextChangedListener
 import com.capstone.pet2home.R
+import com.capstone.pet2home.databinding.ActivityPostAddBinding
+import com.capstone.pet2home.rotateBitmap
+import com.capstone.pet2home.ui.camera.CameraActivity
+import com.capstone.pet2home.uriToFile
+import java.io.File
 
 class PostAddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPostAddBinding
+    private var itemsGender: Array<String> = arrayOf("Dog","Cat")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityPostAddBinding.inflate(layoutInflater)
         val actionBar: ActionBar? = supportActionBar
         actionBar?.apply {
             setTitle(R.string.title_post_add)
             setDisplayHomeAsUpEnabled(true)
             setHomeAsUpIndicator(R.drawable.ic_arrow_back_24)
         }
-        setContentView(R.layout.activity_post_add)
+        setContentView(binding.root)
+
+        setMyButtonEnable()
+        viewSetupInputDropdownGender()
+        buttonListener()
+    }
+
+    private fun buttonListener(){
+        binding.btnChangePhoto.setOnClickListener{
+            chooseProfilPicture()
+        }
+
+        binding.btnPosting.setOnClickListener{
+
+        }
+    }
+
+    private fun chooseProfilPicture(){
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        val inflater: LayoutInflater = layoutInflater
+        val dialogView: View = inflater.inflate(R.layout.alert_dialog_picture, null)
+        builder.setCancelable(true)
+        builder.setView(dialogView)
+
+        val btnCamera: ImageView = dialogView.findViewById(R.id.btnCamera)
+        val btnGalery: ImageView = dialogView.findViewById(R.id.btnGallery)
+
+        val alertDialogPicture: AlertDialog = builder.create()
+        alertDialogPicture.show()
+
+        btnCamera.setOnClickListener{
+            if(checkAndRequestPermission()){
+                val intent = Intent(this, CameraActivity::class.java)
+                launcherIntentCameraX.launch(intent)
+            }
+        }
+
+        btnGalery.setOnClickListener{
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image/*"
+            val chooser = Intent.createChooser(intent, "Choose a Picture")
+            launcherIntentGallery.launch(chooser)
+        }
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if (it.resultCode == CAMERA_X_RESULT){
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
+            val result = rotateBitmap(BitmapFactory.decodeFile(myFile.path), isBackCamera)
+
+            binding.imageUserAvatar.setImageBitmap(result)
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+            result ->
+        if (result.resultCode == RESULT_OK){
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, this)
+            binding.imageUserAvatar.setImageURI(selectedImg)
+        }
+    }
+
+    private fun viewSetupInputDropdownGender() {
+        val adapterItems: ArrayAdapter<String> = ArrayAdapter(this,R.layout.list_item_gender,itemsGender)
+        binding.apply {
+            autoCompleteJenisHewan.setAdapter(adapterItems)
+            autoCompleteJenisHewan.onItemClickListener =
+                AdapterView.OnItemClickListener { parent, _, position, _ ->
+                    val item: String = parent.getItemAtPosition(position).toString()
+                    Toast.makeText(applicationContext, "Item : $item",Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    private fun setMyButtonEnable(){
+        binding.edtNamaHewan.addTextChangedListener(onTextChanged = { _, _, _, _ ->
+            enableButton()
+        })
+        binding.autoCompleteJenisHewan.addTextChangedListener(onTextChanged = { _, _, _, _ ->
+            enableButton()
+        })
+        binding.edtWhatshapp.addTextChangedListener(onTextChanged = {_, _, _, _ ->
+            enableButton()
+        })
+        binding.edtDeskripsi.addTextChangedListener(onTextChanged = {_, _, _, _ ->
+            enableButton()
+        })
+    }
+
+    private fun enableButton() {
+        val nameHewan = binding.edtNamaHewan.text
+        val jenisHewan = binding.autoCompleteJenisHewan.text
+        val nomerWhatsapp = binding.edtWhatshapp.text
+        val deskripsi = binding.edtWhatshapp.text
+
+        binding.btnPosting.isEnabled =
+            nameHewan != null && jenisHewan != null && nomerWhatsapp != null && deskripsi != null && binding.edtNamaHewan.text.toString().length >= 6 && binding.edtWhatshapp.text.toString().length >= 10 && binding.edtDeskripsi.text.toString().length >= 6
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -24,5 +153,37 @@ class PostAddActivity : AppCompatActivity() {
             return true
         }
         return super.onContextItemSelected(item)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+
+            Toast.makeText(this, "mendapatkan permission.", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this, "Tidak mendapatkan permission.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkAndRequestPermission(): Boolean{
+        if (Build.VERSION.SDK_INT >= 23){
+            val cameraPermission: Int = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            if(cameraPermission == PackageManager.PERMISSION_DENIED){
+                ActivityCompat.requestPermissions(this,
+                    REQUIRED_PERMISSIONS,
+                    REQUEST_CODE_PERMISSIONS
+                )
+                return false
+            }
+        }
+        return true
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 20
     }
 }
